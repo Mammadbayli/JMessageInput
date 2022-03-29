39a47e8846f852da1578fce58abffc35a96d6526
Javad Mammadbayli <jmemmed@gmail.com>
2022-03-29 12:24:58 +0400

    Initial commit

---
+++ Example/JMessageInput/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  JMessageInput
+//
+//  Created by mammadbayli on 03/29/2022.
+//  Copyright (c) 2022 mammadbayli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
