b07c15030988ac7b081eb55d935fadb6b2a98d37
Javad <javad@mammadbayli.com>
2025-02-03 21:08:50 -0800

    fix example project

--- Example/JMessageInput/ViewController.swift
+++ Example/JMessageInput/ViewController.swift
@@ -11,42 +11,75 @@ import JMessageInput
 
 class ViewController: UIViewController {
     
-    let input = JMessageInput()
-    var bottomConstraint: NSLayoutConstraint?
+    private lazy var input: JMessageInput = {
+        let input = JMessageInput()
+        input.textView.layer.borderColor = UIColor.systemBlue.withAlphaComponent(1).cgColor
+        input.textView.layer.borderWidth = 0.5
+
+        input.delegate = self
+
+        return input
+    }()
+
+    private var bottomConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        self.view.addSubview(input)
-        self.view.translatesAutoresizingMaskIntoConstraints = false
+
+        let chatArea = UIView()
+        chatArea.backgroundColor = UIColor.systemBlue.withAlphaComponent(0.3)
+        chatArea.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(chatArea)
+        view.addSubview(input)
 
         if #available(iOS 11.0, *) {
             bottomConstraint = input.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
             
             NSLayoutConstraint.activate([
-                bottomConstraint!,
+                bottomConstraint,
                 input.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
                 input.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-                //                input.heightAnchor.constraint(equalToConstant: 50)
+                chatArea.bottomAnchor.constraint(equalTo: input.bottomAnchor),
+                chatArea.topAnchor.constraint(equalTo: view.topAnchor),
+                chatArea.widthAnchor.constraint(equalTo: view.widthAnchor),
+                chatArea.centerXAnchor.constraint(equalTo: view.centerXAnchor)
             ])
         } else {
             // Fallback on earlier versions
         }
         
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(self.keyboardWillHide(notification:)),
-                                               name: UIResponder.keyboardWillHideNotification,
-                                               object: nil)
-        
-        
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(self.keyboardWillShow(notification:)),
-                                               name: UIResponder.keyboardWillShowNotification,
-                                               object: nil)
-        
-        self.navigationController?.interactivePopGestureRecognizer?.delaysTouchesBegan = false
-        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWillHide(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWillShow(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+
+        navigationController?.interactivePopGestureRecognizer?.delaysTouchesBegan = false
+    }
+
+    private lazy var blurView: UIView = {
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        return blurEffectView
+    }()
+
+    override func viewDidLayoutSubviews() {
+
+        blurView.removeFromSuperview()
+        blurView.frame = input.bounds
+        input.insertSubview(blurView, at: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -56,25 +89,32 @@ class ViewController: UIViewController {
     
 //
     override var preferredScreenEdgesDeferringSystemGestures: UIRectEdge {
-        return [.left, .bottom, .right]
+        [.left, .bottom, .right]
     }
     
     @objc func keyboardWillShow(notification: Notification) {
         let endFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
         
         if #available(iOS 11.0, *) {
-            bottomConstraint?.constant = -endFrame.height + self.view.safeAreaInsets.bottom
+            bottomConstraint?.constant = -endFrame.height + view.safeAreaInsets.bottom
         } else {
             // Fallback on earlier versions
         }
         
     }
     @objc func keyboardWillHide(notification: Notification) {
-//        let endFrame = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-        
         bottomConstraint?.constant = 0
-        
     }
 
+}
+
+extension ViewController: JMessageInputDelegate {
+    func textDidChange(input: JMessageInput, text: String?) {
+        if text?.isEmpty ?? true {
+            input.state = .initial
+        } else {
+            input.state = .dirty
+        }
+    }
 }
 
